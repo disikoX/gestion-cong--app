dc5320376b799a95d735d9d933069f148c612e8b
disikoX <damienzara.12@gmail.com>
2024-07-14 16:27:18 +0200

    changed some code snippet to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmployeController;
+use App\Http\Controllers\CongeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Routes pour les employés
+Route::get('/employes', [EmployeController::class, 'index'])->name('employes.index');
+Route::get('/employes/{id}', [EmployeController::class, 'show'])->name('employes.show');
+Route::post('/employes', [EmployeController::class, 'store'])->name('employes.store');
+Route::put('/employes/{id}', [EmployeController::class, 'update'])->name('employes.update');
+Route::delete('/employes/{id}', [EmployeController::class, 'destroy'])->name('employes.destroy');
+Route::get('/employes/create', [EmployeController::class, 'create'])->name('employes.create');
+
+
+// Routes pour les congés
+Route::get('/conges', [CongeController::class, 'index'])->name('conges.index');
+Route::get('/conges/{id}', [CongeController::class, 'show'])->name('conges.show');
+Route::post('/conges', [CongeController::class, 'store'])->name('conges.store');
+Route::put('/conges/{id}', [CongeController::class, 'update'])->name('conges.update');
+Route::delete('/conges/{id}', [CongeController::class, 'destroy'])->name('conges.destroy');
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeController;
-use App\Http\Controllers\CongeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,18 +18,3 @@ Route::get('/', function () {
 
 
 
-// Routes pour les employés
-Route::get('/employes', [EmployeController::class, 'index'])->name('employes.index');
-Route::get('/employes/{id}', [EmployeController::class, 'show'])->name('employes.show');
-Route::post('/employes', [EmployeController::class, 'store'])->name('employes.store');
-Route::put('/employes/{id}', [EmployeController::class, 'update'])->name('employes.update');
-Route::delete('/employes/{id}', [EmployeController::class, 'destroy'])->name('employes.destroy');
-Route::get('/employes/create', [EmployeController::class, 'create'])->name('employes.create');
-
-
-// Routes pour les congés
-Route::get('/conges', [CongeController::class, 'index'])->name('conges.index');
-Route::get('/conges/{id}', [CongeController::class, 'show'])->name('conges.show');
-Route::post('/conges', [CongeController::class, 'store'])->name('conges.store');
-Route::put('/conges/{id}', [CongeController::class, 'update'])->name('conges.update');
-Route::delete('/conges/{id}', [CongeController::class, 'destroy'])->name('conges.destroy');
